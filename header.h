5834d5a6f5be89dea804c1cc57faf43c98f46861
jonathanlam10 <jonathanlam10@csu.fullerton.edu>
2023-02-16 13:40:46 -0800

    added syntax analyzer functions to header file

--- header.h
+++ header.h
@@ -4,5 +4,17 @@
 * Last Updated by: Jonathan Lam
 * Contact information: jonathanlam10@csu.fullerton.edu
 * File Purpose: Holds all the classes and functions
-* Classes and Functions for the Analyzers of a Compiler
+* Description: Classes and Functions for the Analyzers of a Compiler
 **/ 
+
+#ifndef HEADER_H_
+#define HEADER_H_
+
+
+//Syntax Analyzer Functions
+char get_token_stream(string)
+int convert_to_syntax_tree(char)
+char return_syntax_tree(string)
+
+
+#endif
