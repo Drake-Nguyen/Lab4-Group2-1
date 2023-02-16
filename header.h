33954cb164b380e5a86adcbb8201ff525bd684b5
jonathanlam10 <jonathanlam10@csu.fullerton.edu>
2023-02-16 13:20:02 -0800

    added commits to header

---
+++ header.h
@@ -0,0 +1,8 @@
+/** 
+* Original Author: Jonathan Lam
+* Created: 2023/02/16  
+* Last Updated by: Jonathan Lam
+* Contact information: jonathanlam10@csu.fullerton.edu
+* File Purpose: Holds all the classes and functions
+* Classes and Functions for the Analyzers of a Compiler
+**/ 
