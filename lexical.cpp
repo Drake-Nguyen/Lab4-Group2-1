31944caf988c590102bcb7299fd9bef83443e775
Long Calamia <long93933@gmail.com>
2023-02-16 13:12:20 -0800

    added comments

---
+++ lexical.cpp
@@ -0,0 +1,8 @@
+/** 
+* Original Author: Long Calamia
+* Created: 2023/02/16  
+* Last Updated by: Long Calamia
+* Contact information: long93933@gmail.com
+* Purpose of this file: Implement a lexical analyzer to take input source code and output token stream.
+* Description of Classes, Functions, etc. in the file. * (c) Copyright/license info 
+**/
