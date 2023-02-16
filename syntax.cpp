6f444b719e03084c1d4d3a43d1fae3472751bfc6
froztty <froztty@github.com>
2023-02-16 14:04:33 -0800

    added comments

--- syntax.cpp
+++ syntax.cpp
@@ -1,13 +1,8 @@
-/** 
+/**
 * Original Author: Nathan Nguyen
-* Created: 2023/02/16  
-* Last Updated by: Nathan Nguyen 
+* Created: 2023/02/16
+* Last Updated by: Nathan Nguyen
 * Contact information: nathannn702@gmail.com
-<<<<<<< HEAD
-* Purpose of this file 
-=======
-* Purpose of this file: Syntax Analyzer that prases through and identify tokens and output a syntax tree
->>>>>>> syntax
-* Description of Classes, Functions, etc. in the file. * (c) Copyright/license info 
+* Purpose of this file Syntax Analyzer that prases through and identify tokens and output a syntax tree
+* Description of Classes, Functions, etc. in the file. * (c) Copyright/license info
 **/ 
-
