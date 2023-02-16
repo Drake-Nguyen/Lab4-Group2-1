b66de2981a8e46fbf656c46afbf9772400a7f376
Drake Nguyen <18dnguyen@csu.fullerton.edu>
2023-02-16 13:24:04 -0800

    changed main.cpp

---
+++ main.cpp
@@ -0,0 +1,8 @@
+/**
+* Original Author: Drake Nguyen
+* Created: 2023/02/16
+* Last Updated by: Drake Nguyen
+* Contact information: 18dnguyen@csu.fullerton.edu
+* Purpose of this file: This file serves as the main functioning file for the compiler.
+* Description of Classes, Functions, etc. in the file. * (c) Copyright/license info 
+**/
