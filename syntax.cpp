8edd6adf3a61398319e6dd685cf112a49a62d89b
dangkyle <dangkyle@csu.fullerton.edu>
2023-02-16 18:44:13 -0800

    change to syntax.cpp

--- syntax.cpp
+++ syntax.cpp
@@ -1,8 +1,17 @@
 /**
 * Original Author: Nathan Nguyen
 * Created: 2023/02/16
-* Last Updated by: Nathan Nguyen
-* Contact information: nathannn702@gmail.com
+* Last Updated by: Kyle Dang
+* Contact information: nathannn702@gmail.com, dangkyle@csu.fullerton.edu
 * Purpose of this file Syntax Analyzer that prases through and identify tokens and output a syntax tree
-* Description of Classes, Functions, etc. in the file. * (c) Copyright/license info
+* Description of Classes, Functions, etc. in the file.
+
+*public variable token_string; will be used to store the token string value from the get_token_stream function
+*public variable syntax_tree; will be used to store the result syntax tree to be returned 
+
+*char get_token_stream(string) - will take the value of token stream from lexical.cpp 
+*int convert to syntax_tree(char) - will convert the token stream value stored into a syntax tree 
+*char return_syntax_tree(string) - returns the syntax tree back to where function was called for use
+
+* (c) Copyright/license info
 **/ 
