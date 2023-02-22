a2f311532c0c2cb7ba532928a67b20b90febdebb
dangkyle <dangkyle@csu.fullerton.edu>
2023-02-21 19:41:46 -0800

    updated the syntax.cpp file

--- syntax.cpp
+++ syntax.cpp
@@ -3,7 +3,7 @@
 * Created: 2023/02/16
 * Last Updated by: Kyle Dang
 * Contact information: nathannn702@gmail.com, dangkyle@csu.fullerton.edu
-* Purpose of this file Syntax Analyzer that prases through and identify tokens and output a syntax tree
+* Purpose of this file - Syntax Analyzer that prases through and identify tokens and output a syntax tree
 * Description of Classes, Functions, etc. in the file.
 
 *public variable token_string; will be used to store the token string value from the get_token_stream function
@@ -15,3 +15,40 @@
 
 * (c) Copyright/license info
 **/ 
+
+#include <iostream>
+#include <string>
+
+class SynA {
+
+    public:
+        std::string token_string;
+
+    private:
+
+};
+
+//Prototypes 
+char get_token_stream(std::string token_string);
+char convert_to_syntax_tree(char token);
+
+// takes token_stream from lexical.cpp and stores in variable 
+char get_token(std::string token_string) {
+
+    char token = 'a';
+    return token;
+
+}
+
+// converts token_stream input argument into a syntax tree
+// returns the finished syntax tree 
+char convert_to_syntax_tree(char token) {
+
+    char syntax_tree_result;
+
+    // convert token to syntax tree here 
+
+    return syntax_tree_result;
+
+}
+
